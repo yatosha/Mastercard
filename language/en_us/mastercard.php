454c1844bfb35872310621fb49a9000a59ee28a8
yatosha <josephh@yatosha.com>
2018-11-10 11:48:28 +0300

    All files

---
+++ language/en_us/mastercard.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * PusulaSoft
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the PusulaSoft Magento Extension License
+ * that is available at: http://store.pusulasoft.com.tr/license/extension/magento
+ * 
+ * if you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@pusulasoft.com.tr so we can send you a copy immediately.
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade the extension
+ * to newer versions in the future. If you wish to customize the extension
+ * for your needs please refer to http://store.pusulasoft.com.tr/ for more information
+ * or send an email to satis@pusulasoft.com.tr
+ * 
+ * @category   ConSoft
+ * @package    ConSoft_
+ * @copyright  Copyright (c) 2017 PusulaSoft
+ * @license    http://store.pusulasoft.com.tr/license/extension/magento
+ */
+
+// Gateway name
+$lang['Mastercard.name']                = "Mastercard";
+// Settings
+$lang['Mastercard.version']             = "Version";
+$lang['Mastercard.command']             = "Command";
+$lang['Mastercard.merchant']            = "Merchant Id";
+$lang['Mastercard.access_code']         = "Access Code";
+$lang['Mastercard.merchant_secret_key'] = "Secret Key";
+$lang['Mastercard.gateway_url']         = "Gateway Url";
