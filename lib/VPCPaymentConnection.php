73d916fbc34affa8006076c38b80dc39df670a41
yatosha <josephh@yatosha.com>
2018-11-10 13:01:12 +0300

    Update VPCPaymentConnection.php

--- lib/VPCPaymentConnection.php
+++ lib/VPCPaymentConnection.php
@@ -1,29 +1,8 @@
 <?php
 
 /* 
- * PusulaSoft
- * 
- * NOTICE OF LICENSE
- * 
- * This source file is subject to the PusulaSoft Magento Extension License
- * that is available at: http://store.pusulasoft.com.tr/license/extension/magento
- * 
- * if you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@pusulasoft.com.tr so we can send you a copy immediately.
- * 
- * DISCLAIMER
- * 
- * Do not edit or add to this file if you wish to upgrade the extension
- * to newer versions in the future. If you wish to customize the extension
- * for your needs please refer to http://store.pusulasoft.com.tr/ for more information
- * or send an email to satis@pusulasoft.com.tr
- * 
- * @category   ConSoft
- * @package    ConSoft_
- * @copyright  Copyright (c) 2017 PusulaSoft
- * @license    http://store.pusulasoft.com.tr/license/extension/magento
- */
+/
+//
 
 require(dirname(__FILE__) . DS . 'PaymentCodesHelper.php');
 
