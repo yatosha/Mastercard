48bbfaf4b9570261afe7fd0d05d8321ddc5f3c38
yatosha <josephh@yatosha.com>
2018-11-11 08:27:28 +0300

    currency

--- mastercard.php
+++ mastercard.php
@@ -36,7 +36,7 @@ class Mastercard extends NonmerchantGateway {
     }
 
     public function getCurrencies() {
-        return array("USD", "TZS");
+        return array("USD", "TZS","GBP");
     }
         
     public function setCurrency($currency) {
